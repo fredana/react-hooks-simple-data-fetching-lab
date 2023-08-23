0f6272aeaa8e8bb8c695742ccd890b75b553dedd
Farida NDiaye <faridandiaye60@gmail.com>
2023-08-23 12:32:30 -0400

    SIMPLE DATA FETCHING LAB

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,37 @@
 // create your App component here
+
+import React, { useEffect, useState } from "react";
+
+
+function App() {
+    const [dogImage, setDogImage] = useState(null);
+    const [loading, setLoading] = useState(true);
+  
+    useEffect(() => {
+      // Fetch random dog image
+      fetch("https://dog.ceo/api/breeds/image/random")
+        .then((response) => response.json())
+        .then((data) => {
+          setDogImage(data.message);
+          setLoading(false);
+        })
+        .catch((error) => {
+          console.error("Error fetching dog image:", error);
+          setLoading(false);
+        });
+    }, []);
+  
+    return (
+      <div>
+        <h1>Random Dog Image</h1>
+        {loading ? (
+          <p>Loading...</p>
+        ) : (
+          <img src={dogImage} alt="A Random Dog" />
+        )}
+      </div>
+    );
+  }
+  
+  export default App;
+  
